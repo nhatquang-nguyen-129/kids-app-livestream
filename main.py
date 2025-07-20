fd7209dd15294f61d9ad60f74e45557131a28d63
Nhat Quang Nguyen <nhatquang.nguyen.129@email.com>
2025-07-20 18:13:03 +0700

    📝 Update name

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+# app/main.py
 from fastapi import FastAPI
+from app.routes.livestream import router as livestream_router
 
-app = FastAPI()
+app = FastAPI(
+    title="Livestream Service",
+    version="1.0.0",
+)
+
+app.include_router(livestream_router, prefix="/livestreams", tags=["Livestreams"])
 
 @app.get("/")
-def root():
-    return {"message": "Livestream Service is running"}
+async def root():
+    return {"message": "Livestream service is up and running!"}
